c09a9bab077ccd7cbafb21b5e39090057714cd91
rf5860 <robert.foy@ventyx.abb.com>
2012-08-02 19:51:06 +1000

    Initial commit

---
+++ Kill-AV.ps1
@@ -0,0 +1,7 @@
+Stop-Process -processname McTray
+Stop-Process -processname naPrdMgr
+Stop-Process -processname UdaterUI
+Stop-Process -processname VsTskMgr
+Stop-Process -processname mfeann
+Stop-Process -processname McShield
+Stop-Process -processname EngineServer
